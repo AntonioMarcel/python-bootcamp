6fa74505c3811741ecec52379fede4e5668fd363
SPF-SEPLAG <superintspf@gmail.com>
2025-07-09 16:50:36 -0300

    Review unit testing lessons udemy

---
+++ unit-testing/cap.py
@@ -0,0 +1,6 @@
+import string
+
+def cap_text(text):
+    return string.capwords(text)
+
+print(cap_text("Monty Python's Flying Circus"))
---
+++ unit-testing/simple1.py
@@ -0,0 +1,19 @@
+# a = 1
+# b = 2
+# print(a)
+# print(B)
+
+"""
+A very simple script.
+"""
+
+def myfunc():
+    """
+    An extremely simple function.
+    """
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
